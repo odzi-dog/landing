bb100b715a162539290f30aff78af14a20c73e13
julipup <dingolediego@gmail.com>
2022-10-23 09:34:51 +0000

    Change adapter to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 import astroI18next from "astro-i18next";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   output: "server",
-  adapter: cloudflare()
+  adapter: netlify()
 });
