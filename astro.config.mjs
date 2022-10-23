1d7b1e416cd9a60ee92600b6084c33317eada46f
julipup <dingolediego@gmail.com>
2022-10-23 09:43:03 +0000

    Change deployment platform to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  output: "server",
-  adapter: netlify()
+  site: 'https://odzi-dog.github.io',
+  base: '/landing',
 });
