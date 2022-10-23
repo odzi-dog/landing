6a5973241be86c834c4ec68626520ab44279ee31
julipup <dingolediego@gmail.com>
2022-10-23 09:57:51 +0000

    Update Astro generator config (second try)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  site: 'https://odzi-dog.github.io',
-  base: '/landing',
+  site: 'https://odzi.dog',
+  base: '/',
 });
