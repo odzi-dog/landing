4aceba1c6937b91754e932ad8c359834a9b46a73
julipup <dingolediego@gmail.com>
2022-10-30 19:32:11 +0000

    Added Astro's sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [tailwind(), astroI18next(), sitemap()],
   site: 'https://odzi.dog',
-  base: '/',
+  base: '/'
 });
