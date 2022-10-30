fe68357d697bf5e5510d284d90adb8a6ef15a40a
julipup <dingolediego@gmail.com>
2022-10-30 21:00:15 +0200

    Fix Github link in Socials.const.ts

--- src/config/Socials.const.ts
+++ src/config/Socials.const.ts
@@ -6,7 +6,7 @@ interface Social {
 export const Socials: Array<Social> = [
   {
     text: "github",
-    link: "https://github.com/odzi.dog",
+    link: "https://github.com/odzi-dog",
   },
   {
     text: "docs",
