c0b04dca22d93bd1bd3975a92f61a51df38e64d2
julipup <dingolediego@gmail.com>
2023-08-26 17:40:59 +0300

    Remove all cards, update landing translations, remove Documentation link

--- src/config/Socials.const.ts
+++ src/config/Socials.const.ts
@@ -8,8 +8,8 @@ export const Socials: Array<Social> = [
     text: "github",
     link: "https://github.com/odzi-dog",
   },
-  {
-    text: "docs",
-    link: "https://odzi.dog/docs"
-  }
+  // {
+  //   text: "docs",
+  //   link: "https://odzi.dog/docs"
+  // }
 ];
